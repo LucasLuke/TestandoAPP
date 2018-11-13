c3dfd3f64dc6fbd68af29eeaf8577e116037439b
Lucas Almeida <lucasalmeida_901@hotmail.com>
2018-11-12 22:49:21 -0200

    Adionando projeto de teste

---
+++ www/js/nav.js
@@ -0,0 +1,92 @@
+angular.module('ionicApp', ['ionic'])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    .state('tabs', {
+      url: "/tab",
+      abstract: true,
+      templateUrl: "templates/tabs.html"
+    })
+    .state('tabs.home', {
+      url: "/home",
+      views: {
+        'home-tab': {
+          templateUrl: "templates/home.html",
+          controller: 'HomeTabCtrl'
+        }
+      }
+    })
+    .state('tabs.facts', {
+      url: "/facts",
+      views: {
+        'home-tab': {
+          templateUrl: "templates/facts.html"
+        }
+      }
+    })
+    .state('tabs.medicacao', {
+      url: "/medicacao",
+      views: {
+        'home-tab': {
+          templateUrl: "templates/medicacao.html"
+        }
+      }
+    })
+    .state('tabs.consulta', {
+      url: "/consulta",
+      views: {
+        'home-tab': {
+          templateUrl: "templates/consulta.html"
+        }
+      }
+    })
+    .state('tabs.exame', {
+      url: "/exame",
+      views: {
+        'home-tab': {
+          templateUrl: "templates/exame.html"
+        }
+      }
+    })
+    .state('tabs.facts2', {
+      url: "/facts2",
+      views: {
+        'home-tab': {
+          templateUrl: "templates/facts2.html"
+        }
+      }
+    })
+    .state('tabs.about', {
+      url: "/about",
+      views: {
+        'about-tab': {
+          templateUrl: "templates/about.html"
+        }
+      }
+    })
+    .state('tabs.navstack', {
+      url: "/navstack",
+      views: {
+        'about-tab': {
+          templateUrl: "templates/nav-stack.html"
+        }
+      }
+    })
+    .state('tabs.contact', {
+      url: "/contact",
+      views: {
+        'contact-tab': {
+          templateUrl: "templates/contact.html"
+        }
+      }
+    });
+
+
+   $urlRouterProvider.otherwise("/tab/home");
+
+})
+
+.controller('HomeTabCtrl', function($scope) {
+  console.log('HomeTabCtrl');
+});
